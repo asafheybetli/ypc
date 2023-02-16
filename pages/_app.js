597c9b3a59f7fde9660c03e756978bc08c02c807
ronikox <86534479+asafheybetli@users.noreply.github.com>
2023-02-16 21:25:13 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { StatusProvider } from "../context/statusContext";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <StatusProvider>
       <Component {...pageProps} />
+      <Analytics />
+
     </StatusProvider>
   );
 }
